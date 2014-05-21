b400425468f59831bad42451a62d5024cf9d08e1
Arjun Mehta <arjunmeht@gmail.com>
2014-05-21 16:55:53 -0400

    interprocess function execution

--- main.js
+++ main.js
@@ -6,48 +6,45 @@
 
 var debug = require('debug')('samsaara:ipcRedis');
 var debugMessaging = require('debug')('samsaara:ipcRedis:messaging');
+var debugProcesses = require('debug')('samsaara:ipcRedis:process');
+var debugProcessCallBacks = require('debug')('samsaara:ipcRedis:callback');
 
 var redis = require('redis');
 
 function ipcRedis(options){
 
-
-  var processUuid,
+  var core,
       connectionController,
       communication,
       router,
-      ipc,
       connections;
 
-  var symbolic = require('./symbolic');
-  var SymbolicConnection = symbolic.SymbolicConnection;
+  var Process,
+      SymbolicConnection;
 
   var routes = {},
-      routeList = {};         
+      routeList = {};  
+
+  var processes = {};
 
   var redisSub = redis.createClient(),
       redisPub = redis.createClient(),
       redisClient = redis.createClient();
 
   redisSub.on("message", function (channel, message) {
+    debugMessaging("New IPC Message", channel, message);
     switchMessages(channel, message);
   });
 
 
+  // retrieves a peer process instance.
 
-  /**
-   * Main IPC Router Internal
-   */
-
-  function switchMessages(channel, message){
-    debugMessaging("New Message On Channel", processUuid, channel, message);
-    routes[channel](channel, message);
+  function process(processUuid){
+    return processes[processUuid];
   }
 
 
-  /**
-   * Foundation Methods
-   */
+  // Foundation Methods
 
   function addRoute(routeName, channel, func){
     routes[channel] = func;
@@ -66,10 +63,7 @@ function ipcRedis(options){
   }
 
 
-
-  /**
-   * Base Pub/Sub
-   */
+  // Base Pub/Sub
 
   function publish(channel, message){
     debug("PUBLISH", channel, message);
@@ -94,71 +88,171 @@ function ipcRedis(options){
 
 
 
+  // 
+  // Route Methods (for IPC)
+  // messaging/routing. might need to split this off into its own submodule
+  // 
 
-  /**
-   * Router Methods (for IPC)
-   */
+  // The use of "headers" should be documented. Basically, headers should be used when forwarding messages
+  // to prevent the need to prematurely parse JSON strings, only to have to repackage the JSON object again
+  // anyway. So it's more efficient to parse a header and analyze whether or not to receive it or to forward it.
+  // most of the "handlers" here handle forwarded messages that have modified headers.
+  // An exception where one can use headers may be for messages that are simple, predictable in format and only hold
+  // string or numeric values. This information can be passed through headers alone.
+
+  // Main IPC Router Internal
+
+  function switchMessages(channel, message){
+    routes[channel](channel, message);
+  }
 
 
-  // incoming messages/objects
+  // Adds a new peer process instance to this process for reference. Usually sent by a new process joining the network.
+  //
+  // channel here is a STRING in the form: "PRC:NEW"
+  // message here is a STRING of the form: "peerUuid"
 
-  /* 
-   * This method is executed when a message from a connection is forwarded from another process
-   * to this process, usually in response to a callback request that was sent from this process
-   * to connections on other processes.
-   */
+  function handleNewProcess(channel, peerUuid){
+    debugProcesses("New Process", peerUuid);
+    processes[peerUuid] = new Process(peerUuid);
+
+    // var testMessage = core.uuid + " to " + peerUuid + " sent";
+    // debugProcesses("Testing Process-Process Communication", testMessage);
+
+    // process(peerUuid).execute("testInterprocess", testMessage, function(message){
+    //   debugProcessCallBacks("Testing Process-Process CallBack", testMessage, "CallBack:", message);
+    // });
+  }
+
+  // function testInterprocess(message, callBack){
+  //   debugProcesses("Testing Process-Process Communication", "Received and Executed testInterprocess", message, this.id);
+  //   if(typeof callBack === "function") callBack(core.uuid + " to " + this.id + " sent");
+  // }
+
+  function handleDeadProcess(channel, peerUuid){    
+    delete processes[peerUuid];
+  }
+
+
+  // This method is executed when a message from a connection is forwarded from another process
+  // to this process, usually in response to a callback request that was sent from this process
+  // to connections on other processes.
+  // 
+  // channel here is a STRING in the form: "PRC:ProcessUUID:FWD"
+  // message here is a STRING of the form: "FRM:connectionID::{samsaaraJSONFunctionCall}"
 
   function handleForwardedMessage(channel, message){
 
-    // channel here is a STRING in the form: "PRC:ProcessUUID:FWD"
-    // message here is a STRING of the form: "FRM:connectionID::{samsaaraJSONFunctionCall}"
-
-    debug("Handle Forwarded Message", processUuid, channel, message);
+    debugMessaging("Handle Forwarded Message", core.uuid, channel, message);
 
     var index = message.indexOf("::");
-    var senderInfo = message.substring(0, index);
-    var connMessage = message.slice(2+index-message.length);
+    var header = message.substring(0, index);
+    var headerSplit = header.split(":");
 
-    var senderInfoSplit = senderInfo.split(":");
-    var connID = senderInfoSplit[senderInfoSplit.indexOf("FRM")+1];
+    var connMessage = message.slice(2+index-message.length);    
+    var connID = headerSplit[headerSplit.indexOf("FRM")+1];
 
     var messageObj = parseJSON(connMessage);
 
-    debug("Process Message", processUuid, senderInfoSplit, connID, messageObj);
+    debug("Process Message", core.uuid, headerSplit, connID, messageObj);
 
     if(messageObj !== undefined){
       communication.executeFunction({ connection: connections[connID] || {id: connID} }, messageObj);
     }
   }
 
-  function handleCallBackList(channel, message){   
 
-    // channel here is a STRING in the form: "PRC:ProcessUUID:CBL"
-    // message here is a STRING of the form: "callBackID:connectionID:connectionID:..:connectionID"
+  // Used to handle a list of connections that are expected to return values
+  // to a callBack on this process.
+  //
+  // channel here is a STRING in the form: "PRC:ProcessUUID:CBL"
+  // message here is a STRING of the form: "callBackID:connectionID:connectionID:..:connectionID"
+
+  function handleCallBackList(channel, message){
 
     var callBackListSplit = message.split(":");
     var callBackID = callBackListSplit.shift();
 
-    // debug("ADDING CALL BACK CONNECTIONS", callBackID, callBackListSplit, communication.incomingCallBacks);
+    // debug("Adding CallBack Connections", callBackID, callBackListSplit, communication.incomingCallBacks);
     
     communication.incomingCallBacks[callBackID].addConnections(callBackListSplit || []);
   }
 
-  function handleNewSymbolicConnection(channel, message){    
 
-    // channel here is a STRING in the form: "PRC:ProcessUUID:SYMNEW"
-    // message here is a STRING of the form: {SymbolicData}
+  // Adds a new "symbolic connection" to this process.
+  //
+  // channel here is a STRING in the form: "PRC:ProcessUUID:SYMNEW"
+  // message here is a STRING of the form: {SymbolicData}
+
+  function handleNewSymbolicConnection(channel, message){    
 
     var symbolicData = parseJSON(message);
     var symbolicConnID = symbolicData.nativeID;
-
-    connections[symbolicConnID] = new SymbolicConnection(symbolicData);    
-
-    addRoute("symMsg"+symbolicConnID, "SYM:"+symbolicConnID+":MSG", handleSymbolicMessage);
-    addRoute("symDel"+symbolicConnID, "SYM:"+symbolicConnID+":DEL", handleDeleteSymbolicConnection);
+    connections[symbolicConnID] = new SymbolicConnection(symbolicData);
 
   }
 
+
+  // executes a method call from another process on this process. Taps into the core communication function execution method
+  // with a custom callBackGenerator.
+  //
+  // channel here is a STRING in the form: "PRC:processUUID:EXEC"
+  // message here is a STRING of the form: {owner: processUuid, func: functionName, ns: "interprocess", args: argsArray, callBack: callBackID}
+
+  function handleInterprocessExecute(channel, message){   
+
+    var messageObj = parseJSON(message)[1];
+    messageObj.ns = "interprocess";
+    var executor = {id: messageObj.owner};
+
+    // debugProcesses("Handle New Execute", message, executor);
+
+    communication.executeFunction(executor, messageObj, createIPCCallBack);
+  }
+
+
+  // channel here is a STRING in the form: "PRC:processUUID:IPCCB"
+  // message here is a STRING of the form: {owner: processUuid, func: functionName, ns: "interprocess", args: argsArray, callBack: callBackID}
+
+  function handleIPCCallBack(channel, message){
+
+    // debugProcessCallBacks("Handle IPC CallBack", channel, message);
+
+    var messageObj = parseJSON(message);
+    var executor = {connection: {id: messageObj.sender}}; // need to modify communication.callItBack to accept more than just connections as callback executors
+    communication.executeFunction(executor, messageObj, createIPCCallBack);
+  }
+
+  function createIPCCallBack(id, sender, owner){
+
+    // debugProcessCallBacks("Creating IPC Callback");
+
+    var theCallBack = function(){
+      var args = Array.prototype.slice.call(arguments);
+      publish("PRC:"+owner+":IPCCB", JSON.stringify({ns: "internal", func: "callItBack", args: [id, args], sender: core.uuid}) );
+      delete communication.outgoingCallBacks[id];
+    };
+
+    theCallBack.id = id;
+
+    return theCallBack;
+  }
+
+
+  function sendCallBackList(processID, callBackID, callBackList){
+    // debug("ipcRedis", processUuid, "SENDING CALLBACK LIST ", processID, callBackID, callBackList);
+    publish("PRC:"+processID+":CBL", callBackID+callBackList);
+  }
+
+  function sendCallBackExecute(processID, callBackID, message){
+    publish("PRC:"+processID+":CBX", callBackID+":"+callBackList);
+  }
+
+
+
+
+
+  // symbolic connection handlers
 
   function handleSymbolicMessage(channel, message){
 
@@ -186,9 +280,12 @@ function ipcRedis(options){
     removeRoute("symDel"+symbolicConnID);
   }
 
+
+  // native connection message handlers
+
   function handleMessageToNativeConnection(channel, message){
 
-    // !! need to test this, individual subscriptions to native messages, vs a single router.
+    // !! need to test this, individual subscriptions to native connections, vs. this single router?
 
     // channel here is a STRING in the form: "NTV:connID:MSG"
     // message here is a STRING of the form: "{samsaaraJSONFunctionCall}"
@@ -199,6 +296,8 @@ function ipcRedis(options){
       connections[connID].write(message);
     }
   }
+
+
 
 
   // outgoing messages/objects
@@ -215,35 +314,25 @@ function ipcRedis(options){
     var symbolicData = {
       nativeID: connection.id,
       connectionData: connection.connectionData,
-      owner: processUuid
+      owner: core.uuid
     };
 
     publish("PRC:"+host+":SYMNEW", JSON.stringify(symbolicData) );
   }
 
-  function sendCallBackList(processID, callBackID, callBackList){
-    // debug("ipcRedis", processUuid, "SENDING CALLBACK LIST ", processID, callBackID, callBackList);
-    publish("PRC:"+processID+":CBL", callBackID+callBackList);
-  }
 
-  function sendCallBackExecute(processID, callBackID, message){
-    publish("PRC:"+processID+":CBX", callBackID+":"+callBackList);
-  }
-
-
-
-  /**
-   * Connection Initialization Methods
-   * Called for every new connection
-   *
-   * @opts: {Object} contains the connection's options
-   * @connection: {SamsaaraConnection} the connection that is initializing
-   * @attributes: {Attributes} The attributes of the SamsaaraConnection and its methods
-   */
+  // 
+  // Connection Initialization Methods
+  // Called for every new connection
+  // 
+  // @opts: {Object} contains the connection's options
+  // @connection: {SamsaaraConnection} the connection that is initializing
+  // @attributes: {Attributes} The attributes of the SamsaaraConnection and its methods
+  // 
 
   function connectionInitialzation(opts, connection, attributes){
 
-    debug("Initializing IPC Subscription!!!", processUuid, opts.groups, connection.id);
+    debug("Initializing IPC Subscription!!!", core.uuid, opts.groups, connection.id);
 
     connection.symbolicOwners = {};    
     redisClient.incr("totalCurrentCount");        
@@ -260,12 +349,12 @@ function ipcRedis(options){
   }
 
 
-  /**
-   * Message Router
-   */
+  // 
+  // Message Router
+  // 
 
   function ipcRouteMessage(connection, owner, newHeader, message){        
-    if(owner === processUuid){
+    if(owner === core.uuid){
       router.processMessage(connection, message);
     }
     else{       
@@ -286,41 +375,86 @@ function ipcRedis(options){
   }
 
 
-  /**
-   * Message Filters
-   */
+  function ensureUniqueCoreUuid(uuid, callBack){
+    redisClient.hsetnx("samsaara:processes", uuid, "node", function (err, reply){
+
+      debugProcesses("Unique UUID for Samsaara Process", uuid, err, reply);
+
+      if(err) throw err;
+
+      if(reply === 0){
+        core.uuid = makeIdAlphaNumerical(8);
+        ensureUniqueCoreUuid(core.uuid);        
+      }
+      else{
+        if(typeof callBack === "function") callBack();
+      }
+    });
+  }
 
 
-
-  /**
-   * Module Return Function.
-   * Within this function you should set up and return your samsaara middleWare exported
-   * object. Your eported object can contain:
-   * name, foundation, remoteMethods, connectionInitialization, connectionClose
-   */
+  // Module Return Function.
+  //
+  // Within this function you should set up and return your samsaara middleWare exported
+  // object. Your eported object can contain:
+  // name, foundation, remoteMethods, connectionInitialization, connectionClose
 
   return function ipcRedis(samsaaraCore){
 
-    processUuid = samsaaraCore.uuid;
+    core = samsaaraCore;
+
     connectionController = samsaaraCore.connectionController;
     communication = samsaaraCore.communication;
     router = samsaaraCore.router;
-    ipc = samsaaraCore.ipc;
     connections = connectionController.connections;
 
-    symbolic.initialize(samsaaraCore);
+    SymbolicConnection = require('./symbolic').initialize(samsaaraCore);
+    Process = require('./process').initialize(samsaaraCore, processes, publish);
 
-    // handles messages that are redirected to this process
-    addRoute("process", "PRC:"+processUuid+":FWD", handleForwardedMessage);
+    // core.samsaara.createNamespace("interprocess", {
+    //   testInterprocess: testInterprocess
+    // });
 
-    // handles a list of connections that we are expecting a callback from
-    addRoute("callBackList", "PRC:"+processUuid+":CBL", handleCallBackList);
+    // handles a new process on the system
+    addRoute("newProcess", "PRC:NEW", handleNewProcess);
+    addRoute("deleteProcess", "PRC:DEL", handleDeadProcess);
 
-    // creates a new symbolic connection
-    addRoute("addSymbolicConnection", "PRC:"+processUuid+":SYMNEW", handleNewSymbolicConnection);
+    ensureUniqueCoreUuid(core.uuid, function(){
 
-    // creates a new symbolic connection
-    addRoute("deleteSymbolicConnection", "PRC:"+processUuid+":SYMDEL", handleDeleteSymbolicConnection);
+      publish("PRC:NEW", core.uuid);
+
+      // handles messages that are redirected to this process
+      addRoute("process", "PRC:"+core.uuid+":FWD", handleForwardedMessage);
+
+      // handles a list of connections that we are expecting a callback from
+      addRoute("callBackList", "PRC:"+core.uuid+":CBL", handleCallBackList);
+
+      // creates a new symbolic connection
+      addRoute("addSymbolicConnection", "PRC:"+core.uuid+":SYMNEW", handleNewSymbolicConnection);
+
+      // creates a new symbolic connection
+      addRoute("deleteSymbolicConnection", "PRC:"+core.uuid+":SYMDEL", handleDeleteSymbolicConnection);
+
+      // handles a generic process function execution request
+      addRoute("executeProcessFunction", "PRC:"+core.uuid+":EXEC", handleInterprocessExecute);
+
+      // handles a generic process function execution request
+      addRoute("executeProcessCallBack", "PRC:"+core.uuid+":IPCCB", handleIPCCallBack);
+      
+
+      // get a list of all processes
+      redisClient.hkeys("samsaara:processes", function (err, replies){
+
+        debugProcesses("samsaara Process List", replies);
+
+        for (var i = 0; i < replies.length; i++) {          
+          if(replies[i] !== core.uuid){
+            processes[replies[i]] = new Process(replies[i]);
+          }
+        }
+      });
+    });
+
 
     var exported = {
 
@@ -338,11 +472,12 @@ function ipcRedis(options){
         subscribePattern: subscribePattern,
         unsubscribePattern: unsubscribePattern,
         routes: routes,
-        store: redisClient
+        store: redisClient,
+        process: process
       },
 
       main: {
-        createSymbolicOnHost: createSymbolicOnHost,
+        createSymbolicOnHost: createSymbolicOnHost
       },
 
       connectionInitialization: {
@@ -377,4 +512,14 @@ function parseJSON(jsonString){
   }
 
   return parsed;
+}
+
+
+function makeIdAlphaNumerical(idLength){
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  for( var i=0; i < idLength; i++ ){
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
 }
---
+++ process.js
@@ -0,0 +1,61 @@
+/*!
+ * samsaaraSocks - Context Constructor
+ * Copyright(c) 2013 Arjun Mehta <arjun@newlief.com>
+ * MIT Licensed
+ */
+
+
+var core, 
+    samsaara,
+    communication;
+
+var processes,
+    publish;
+
+
+function initialize(samsaaraCore, processesObj, publishMethod){
+
+  core = samsaaraCore;
+  samsaara = samsaaraCore.samsaara;
+  communication = samsaaraCore.communication;  
+
+  processes = processesObj;
+  publish = publishMethod;
+ 
+  return Process;
+}
+
+
+function Process(processUuid){
+  this.id = this.processUuid = processUuid;
+}
+
+
+Process.prototype.execute = function(){
+
+  var process = this;
+
+  var packet = {owner: core.uuid, ns:"interprocess", func: arguments[0], args: []};
+
+  for (var i = 1; i < arguments.length-1; i++){
+    packet.args.push(arguments[i]);
+  }
+
+  if(typeof arguments[arguments.length-1] === "function"){
+    communication.makeCallBack(0, packet, arguments[arguments.length-1], function (incomingCallBack, packetReady){
+      publish("PRC:"+process.id+":EXEC", packetReady);
+      incomingCallBack.addConnection(process.id);
+    });
+  }
+  else{
+    packet.args.push(arguments[arguments.length-1]);
+    publish("PRC:"+process.id+":EXEC", JSON.stringify(packet));
+  }
+};
+
+
+
+module.exports = exports = {
+  initialize: initialize,
+  Process: Process
+};
